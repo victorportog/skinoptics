a0c40315e544e49e70ee4ea9c16b0324231b2eb2
victorportog <victorporto@ifsc.usp.br>
2024-10-18 11:06:08 -0300

    New version 0.0.1b4

--- skinoptics/__init__.py
+++ skinoptics/__init__.py
@@ -27,6 +27,7 @@ Copyright (C) 2024 Victor Lima
 | 0.0.1b1 - release date: 2024 October 10
 | 0.0.1b2 - release date: 2024 October 18
 | 0.0.1b3 - release date: 2024 October 18
+| 0.0.1b4 - release date: 2024 October 18
 '''
 
 __all__ = ['utils', 'dataframes',
@@ -37,7 +38,7 @@ __all__ = ['utils', 'dataframes',
 __version__major = '0'
 __version__minor = '0'
 __version__micro = '1'
-__version__suffix = 'b3'
+__version__suffix = 'b4'
 
 __author__ = "Victor Lima"
 __copyright__ = "Copyright (c) 2024 Victor Lima"
